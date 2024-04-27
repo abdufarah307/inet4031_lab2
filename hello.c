7b55e603fadc2430ef36865e2e503d59ddf3ed9d
abdufarah307 <162871769+abdufarah307@users.noreply.github.com>
2024-04-27 12:21:50 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,13 +1,15 @@
-  #include <stdio.h>
-              
-               int main() {
-              
-                 int a = 2;
-                 int b = 2;
-                 int c = a + b;
-              
-                 printf("C says: Hello, World!\n");
-                 printf("%d + %d = %d\n",a,b,c);
-              
-                 return 0;
+#include <stdio.h>
+
+int main() {
+    // Creating an array of strings in C
+    const char *users[] = {"User1", "User2", "User2"};
+
+    int a = 2;
+    int b = 2;
+    int c = a + b;
+
+    printf("C says: Hello, World!\n");
+    printf("%d + %d = %d\n", a, b, c);
+
+    return 0;
 }
