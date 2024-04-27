3e8fc207b5f2d02690e46222af30be8202be078d
abdufarah307 <162871769+abdufarah307@users.noreply.github.com>
2024-04-27 12:22:38 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+# Creating an array of strings in Python
+users = ['User1', 'User2', 'User2']
+
 a = 2
 b = 2
 c = a + b
 
 print("Python says: Hello, World!")
-print("%s + %s = %s" % (a,b,c))
-
+print("%s + %s = %s" % (a, b, c))
